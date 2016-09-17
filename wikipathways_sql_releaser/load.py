6f33c5057b22a4ff229cff59b85bca97b92ce58f
Anders Riutta <git@andersriutta.com>
2016-09-17 15:22:13 -0700

    Improve loading code.

--- wikipathways_sql_releaser/load.py
+++ wikipathways_sql_releaser/load.py
@@ -1,37 +1,54 @@
+import os.path
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
 from sqlalchemy.sql import text
 
-metadata = MetaData()
-nodes = Table('nodes', metadata,
-              Column('id', String, primary_key=True),
-              Column('label', String),
-              Column('source', String),
-              )
-
-engine = create_engine('sqlite:///pathways.db')
-metadata.create_all(engine)
-
-inspector = inspect(engine)
+# keeping this here in case I need it again
+# from sqlalchemy import inspect
+# inspector = inspect(engine)
 # print inspector.get_columns('nodes')
 
-with engine.connect() as con:
-    data = ({'id': 'http://identifiers.org/cas/6912-68-1',
-             'label': 'b-Alanine',
-             'source': 'Enzyme Nomenclature'},
-            {'id': 'http://identifiers.org/ec-code/2.1.3.2',
-             'label': '2.1.3.2',
-             'source': 'Enzyme Nomenclature'},
-            )
 
-    statement = text('''INSERT INTO nodes(id, label, source) VALUES(:id, :label, :source)''')
+# create or get
+def init_pathways_engine():
+    location = 'pathways.db'
+    connection_string = 'sqlite:///' + location
+    engine = create_engine(connection_string)
 
-    for line in data:
-        con.execute(statement, **line)
+    if os.path.isfile(location):
+        return engine
 
-    rs = con.execute('SELECT * FROM nodes')
+    metadata = MetaData()
+    Table('nodes', metadata,
+          Column('id', String, primary_key=True),
+          Column('label', String),
+          Column('source', String),
+          )
+    metadata.create_all(engine)
+    return engine
 
-    for row in rs:
-        print row
+
+def load(data):
+    engine = init_pathways_engine()
+    with engine.connect() as con:
+        statement = text('''INSERT INTO nodes(id, label, source)
+                            VALUES(:id, :label, :source)''')
+
+        for line in data:
+            con.execute(statement, **line)
+
+        rs = con.execute('SELECT * FROM nodes')
+
+        for row in rs:
+            print row
+
+
+data = ({'id': 'http://identifiers.org/cas/6912-68-1',
+         'label': 'b-Alanine',
+         'source': 'Enzyme Nomenclature'},
+        {'id': 'http://identifiers.org/ec-code/2.1.3.2',
+         'label': '2.1.3.2',
+         'source': 'Enzyme Nomenclature'},
+        )
+load(data)
